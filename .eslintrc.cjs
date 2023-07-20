875e22db561feb5bda1895bc05169f789d8d1183
Diego Lima <xxdiegolsxx@gmail.com>
2023-07-20 14:49:53 -0300

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,84 @@
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es2021": true,
+//         "node": true
+//     },
+//     "extends": [
+//         "eslint:recommended",
+//         "plugin:@typescript-eslint/recommended",
+//         "plugin:react/recommended"
+//     ],
+//     "overrides": [
+//         {
+//             "env": {
+//                 "node": true
+//             },
+//             "files": [
+//                 ".eslintrc.{js,cjs}"
+//             ],
+//             "parserOptions": {
+//                 "sourceType": "script"
+//             }
+//         }
+//     ],
+//     "parser": "@typescript-eslint/parser",
+//     "parserOptions": {
+//         "ecmaVersion": "latest",
+//         "sourceType": "module"
+//     },
+//     "plugins": [
+//         "@typescript-eslint",
+//         "react"
+//     ],
+//     "rules": {
+//     }
+// }
+
+module.exports = {
+  env: {
+    browser: true,
+
+    es2021: true,
+
+    node: true,
+  },
+
+  extends: [
+    'eslint:recommended',
+
+    'plugin:react/recommended',
+
+    'plugin:prettier/recommended',
+
+    'plugin:react/jsx-runtime',
+
+    'plugin:@typescript-eslint/recommended',
+  ],
+
+  overrides: [],
+
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+
+    sourceType: 'module',
+
+    ecmaFeatures: {
+      jsx: 'true',
+    },
+  },
+
+  plugins: ['react', '@typescript-eslint'],
+
+  rules: {
+    'no-unused-vars': 1,
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
